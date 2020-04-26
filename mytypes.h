6da2dd46118f281d767619700b2226a4e55f67eb
Bart Zuidgeest <dummy@test.nl>
2020-04-26 21:21:00 +0200

    added mytypes header file

---
+++ mytypes.h
@@ -0,0 +1,15 @@
+
+#ifndef MYTYPES_H
+#define MYTYPES_H
+
+#ifndef BYTE_DEFINED
+#define BYTE_DEFINED
+typedef unsigned char byte;
+#endif
+
+#ifndef USHORT_DEFINED
+#define USHORT_DEFINED
+typedef unsigned short ushort;
+#endif
+
+#endif /* MYTYPES_H */
